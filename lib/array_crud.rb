a0ae18aff745ff32277f15cc219d35afb086aea2
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-12 21:09:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,27 +7,27 @@ def create_an_array = ["blue", "red", "green", "yellow"]
 end
 
 def add_element_to_end_of_array(array, element)
-    create_an_array.push(element) 
+    array.push(element) 
 end
 
 def add_element_to_start_of_array(array, element)
-    create_an_array.unshift(element)
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-    create_an_array.pop
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-    create_an_array.shift
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-      
+    index_number[0]  
 end
 
 def retrieve_first_element_from_array(array)
-  
+      
 end
 
 def retrieve_last_element_from_array(array)
